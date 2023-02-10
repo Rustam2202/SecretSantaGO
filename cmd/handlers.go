4d68331194fcef15355a8edfcf1b41654199904a
Rustam2202 <91486552+Rustam2202@users.noreply.github.com>
2023-02-10 13:45:25 +0300

    refactoring handlers

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -1,3 +1,29 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
 
+	"golang.org/x/crypto/bcrypt"
+)
+
+func (app *Application) homeHandler(w http.ResponseWriter, r *http.Request) {
+	app.tpl.ExecuteTemplate(w, "index.html", nil)
+}
+
+func (app *Application) registerHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("*** registerHandler running ***")
+	app.tpl.ExecuteTemplate(w, "register.html", nil)
+}
+
+func (app *Application) registerAuthHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("*** registerAuthHandler running ***")
+	r.ParseForm()
+	email := r.FormValue("email")
+	password := r.FormValue("password")
+	firstName := r.FormValue("firstName")
+	lastName := r.FormValue("lastName")
+	hashPassw, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	app.db.AddPerson(email, firstName, lastName, hashPassw, nil, nil)
+	app.tpl.ExecuteTemplate(w, "index.html", nil)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,9 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"santa/pkg/models/postgres"
+	"santa/pkg/postgres"
 
 	_ "github.com/lib/pq"
-	"golang.org/x/crypto/bcrypt"
 )
 
 type Application struct {
@@ -25,29 +24,7 @@ func main() {
 	http.HandleFunc("/register", app.registerHandler)
 	http.HandleFunc("/registerAuth", app.registerAuthHandler)
 
+	// 'go run ./cmd' from root dir, thats compile both files (main.go and handlers.go)
 	fmt.Println("*** Listen and serve ***")
 	http.ListenAndServe("localhost:8080", nil)
-}
-
-func (app *Application) homeHandler(w http.ResponseWriter, r *http.Request) {
-	app.tpl.ExecuteTemplate(w, "index.html", nil)
-}
-
-func (app *Application) registerHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** registerHandler running ***")
-	app.tpl.ExecuteTemplate(w, "register.html", nil)
-}
-
-func (app *Application) registerAuthHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("*** registerAuthHandler running ***")
-	r.ParseForm()
-	email := r.FormValue("email")
-	password := r.FormValue("password")
-	firstName := r.FormValue("firstName")
-	lastName := r.FormValue("lastName")
-	//fmt.Println(email, password, firstName, lastName)
-	hashPassw, _ := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	app.db.AddPerson(email, firstName, lastName, hashPassw, nil, nil)
-	// w.Write([]byte("You been registrated"))
-	app.tpl.ExecuteTemplate(w, "index.html", nil)
 }
